b488511461d33d1869bdaf383dad78d4a59a6171
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-08 14:03:19 +0100

    Update route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import { scanEtsy } from "@/lib/etsyScanner";
+import { scanEtsy } from "../../../lib/etsyScanner";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
