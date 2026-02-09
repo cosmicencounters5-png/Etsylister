44e562975c6b3c1d7ebcab6db18d484a49a41a34
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-09 23:14:26 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { supabase } from "../../lib/supabaseClient"
+import AuthGuard from "../../components/AuthGuard"
 
 export default function Home(){
 
