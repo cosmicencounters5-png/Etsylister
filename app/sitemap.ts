4e5f41ed8aba53a032049a95c509470a60db4f0b
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-10 09:51:19 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,27 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
 
-  const baseUrl = "https://yourdomain.com"
+  const baseUrl = "https://etsylister.com"
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 1
+      priority: 1.0,
     },
     {
       url: `${baseUrl}/login`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.8
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/dashboard`,
       lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.9
+      priority: 0.9,
     }
   ]
 }
