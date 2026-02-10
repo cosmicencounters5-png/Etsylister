0a0232cd59bf01512c7a7154052f0ee312182316
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-10 10:05:16 +0100

    Create seoKeywords.ts

---
+++ lib/seoKeywords.ts
@@ -0,0 +1,28 @@
+export const baseKeywords = [
+
+  "crochet pattern",
+  "knitting pattern",
+  "digital planner",
+  "printable wall art",
+  "svg bundle",
+  "etsy stickers",
+  "wedding invitation template",
+  "baby shower printable",
+  "business logo template",
+  "social media template",
+  "etsy digital download",
+  "planner stickers",
+  "printable calendar",
+  "etsy gift tags"
+
+]
+
+export const seoTemplates = [
+
+  "etsy listing generator for",
+  "etsy seo for",
+  "best etsy tags for",
+  "etsy keyword strategy for",
+  "etsy title ideas for"
+
+]
