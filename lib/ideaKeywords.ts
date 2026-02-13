496c1c5e422bd06cb5c9ffbc1e7176105bfb5bae
cosmicencounters5-png <cosmicencounters5@gmail.com>
2026-02-13 12:23:15 +0100

    Create ideaKeywords.ts

---
+++ lib/ideaKeywords.ts
@@ -0,0 +1,19 @@
+export const ideaKeywords = [
+
+  "crochet pattern ideas",
+  "etsy printable planner",
+  "digital wall art niche",
+  "wedding invitation template",
+  "baby shower printable",
+  "etsy digital stickers",
+  "budget planner printable",
+  "ai prompt planner",
+  "canva template business",
+  "etsy journal template",
+  "etsy crochet pdf pattern",
+  "etsy notion template",
+  "etsy resume template",
+  "etsy social media kit",
+  "etsy logo template"
+
+]
